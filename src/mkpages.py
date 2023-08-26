844ad964b7be4d26d27340552051264a8d859b1a
Masanori Kusunoki <masanork@gmail.com>
2023-08-27 08:45:17 +0900

    ページタイトルを追加、インディックスのソートロジックを見直し

--- src/mkpages.py
+++ src/mkpages.py
@@ -18,31 +18,37 @@ def convert_md_to_html(md_path, template_path='template.html'):
         md_content = f.read()
         html_content = markdown.markdown(md_content)
         
+        # Markdownファイルからタイトルを取得
+        page_title = get_title_from_md(md_path)
+        if not page_title:  # タイトルが見つからない場合のデフォルト値
+            page_title = "Your Title Here"
+
         # Jinja2テンプレートの読み込み
         env = Environment(loader=FileSystemLoader('./'))
         template = env.get_template(template_path)
-        rendered_html = template.render(title="Your Title Here", content=html_content)
+        rendered_html = template.render(title=page_title, content=html_content)
     return rendered_html
 
 def generate_index_page(files, src_dir, template_path='index_template.html'):
     """index.htmlを生成する（Jinja2を使用）"""
-    # 更新日でファイルをソート
-    files.sort(key=lambda x: get_file_modification_date(os.path.join(src_dir, x)), reverse=True)
+    # 作成日でファイルをソート
+    files.sort(key=lambda x: get_file_creation_date(os.path.join(src_dir, x)), reverse=True)
 
     links = ""
     for file in files:
         md_path = os.path.join(src_dir, file)
         title = get_title_from_md(md_path)
+        creation_date = get_file_creation_date(md_path)
         last_updated = get_file_modification_date(md_path)
+
+        # 作成日と最終更新日が異なる場合、両方を表示
+        if creation_date != last_updated:
+            date_str = f'作成日: {creation_date}, 最終更新: {last_updated}'
+        else:
+            date_str = f'作成日: {creation_date}'
+
         html_filename = os.path.splitext(file)[0] + '.html'
-        links += f'<p><a href="{html_filename}">{title}</a> (最終更新: {last_updated})</p>'
-    
-    # Jinja2テンプレートの読み込み
-    env = Environment(loader=FileSystemLoader('./'))
-    template = env.get_template(template_path)
-    rendered_html = template.render(title="記事一覧", content=links)
-    
-    return rendered_html
+        links += f'<p><a href="{html_filename}">{title}</a> ({date_str})</p>'
     
     # Jinja2テンプレートの読み込み
     env = Environment(loader=FileSystemLoader('./'))
@@ -64,6 +70,11 @@ def get_title_from_md(md_path):
             if heading:
                 return heading.get_text()
     return None
+
+def get_file_creation_date(filepath):
+    """ファイルの作成日を取得する"""
+    create_time = os.path.getctime(filepath)
+    return time.strftime('%Y-%m-%d', time.localtime(create_time))
 
 def get_file_modification_date(filepath):
     """ファイルの更新日を取得する"""
