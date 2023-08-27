d30237b53ed5cee2a81ca0a68b62c1b4712bbdb5
Masanori Kusunoki <masanork@gmail.com>
2023-08-27 09:42:07 +0900

    index、title周りの調整

--- src/mkpages.py
+++ src/mkpages.py
@@ -35,39 +35,35 @@ def convert_md_to_html(md_path, template_path='template.html'):
         back_to_top_link = '<a href="index.html">戻る</a>'
         html_content = back_to_top_link + html_content + footer
         
+        # Markdownファイルからの見出しを取得してHTMLのタイトルに設定
+        page_title = get_title_from_md(md_path)
+
         # Jinja2テンプレートの読み込み
         env = Environment(loader=FileSystemLoader('./'))
         template = env.get_template(template_path)
-        rendered_html = template.render(title="Your Title Here", content=html_content)
+        rendered_html = template.render(title=page_title, content=html_content)
     return rendered_html
 
 def generate_index_page(files, src_dir, template_path='index_template.html'):
     """index.htmlを生成する（Jinja2を使用）"""
-    # 作成日でファイルをソート
-    files.sort(key=lambda x: get_creation_date_from_filename(x) or "", reverse=True)
+    # 更新日でファイルをソート
+    files.sort(key=lambda x: get_file_modification_date(os.path.join(src_dir, x)), reverse=True)
 
     links = ""
     for file in files:
         md_path = os.path.join(src_dir, file)
         title = get_title_from_md(md_path)
-        creation_date = get_creation_date_from_filename(file)
         last_updated = get_file_modification_date(md_path)
-
-        # 作成日と最終更新日が異なる場合、両方を表示
-        if creation_date and creation_date != last_updated:
-            date_str = f'作成日: {creation_date}, 最終更新: {last_updated}'
-        elif creation_date:
-            date_str = f'作成日: {creation_date}'
-        else:
-            date_str = f'最終更新: {last_updated}'
-
         html_filename = os.path.splitext(file)[0] + '.html'
-        links += f'<p><a href="{html_filename}">{title}</a> ({date_str})</p>'
+        links += f'<p><a href="{html_filename}">{title}</a> (最終更新: {last_updated})</p>'
     
+    config = load_config()
+    index_title = config["contents"]["title"]  # config.tomlからタイトルを取得
+
     # Jinja2テンプレートの読み込み
     env = Environment(loader=FileSystemLoader('./'))
-    template = env.get_template(template_path)
-    rendered_html = template.render(title="記事一覧", content=links)
+    template = env.get_template(os.path.basename(template_path))
+    rendered_html = template.render(title=index_title, content=links)
     
     return rendered_html
 
